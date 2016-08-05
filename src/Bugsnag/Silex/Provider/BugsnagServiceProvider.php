35ad01e625cddfa930b10ca847342943b6a863b7
Alan Bondarchuk <imacoda@gmail.com>
2016-08-05 14:44:26 +0500

    Don't send session and cookies as metadata. Minor improvements.

--- src/Bugsnag/Silex/Provider/BugsnagServiceProvider.php
+++ src/Bugsnag/Silex/Provider/BugsnagServiceProvider.php
@@ -2,42 +2,43 @@
 
 namespace Bugsnag\Silex\Provider;
 
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
+use Bugsnag\Client;
+use Bugsnag\Handler;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 class BugsnagServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * The package version.
+     *
+     * @var string
+     */
+    const VERSION = '1.0.0';
+
     public function register(Container $app)
     {
         $app['bugsnag'] = function ($app) {
-            $client = \Bugsnag\Client::make($app['bugsnag.options']['apiKey']);
-            \Bugsnag\Handler::register($client);
+            $client = Client::make($app['bugsnag.options']['apiKey']);
+            $client->setNotifier([
+                'name'    => 'Silex Bugsnag',
+                'version' => static::VERSION,
+                'url'     => 'https://github.com/fortis/silex-bugsnag',
+            ]);
+
+            Handler::register($client);
 
             return $client;
         };
 
         $app->error(
           function (\Exception $error, Request $request) use ($app) {
-              $params['request'] = array(
-                'params' => $request->query->all(),
+              $params['request'] = [
+                'params'        => $request->query->all(),
                 'requestFormat' => $request->getRequestFormat(),
-              );
-
-              if ($session = $request->getSession()) {
-                  $params['session'] = $session->all();
-              }
-
-              if ($cookies = $request->cookies->all()) {
-                  $params['cookies'] = $cookies;
-              }
-
-              $app['bugsnag']->registerCallback(
-                function ($report) use ($params, $error) {
-                    $report->setMetaData($params);
-                }
-              );
+              ];
 
               $app['bugsnag']->notifyException($error);
           }
